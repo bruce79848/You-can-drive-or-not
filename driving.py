0a1267f4de41c4823503b2a9ff8131fbc0c0c6dc
Bruce <bruce79848@gmail.com>
2020-03-09 18:13:24 +0800

    delete country options and add else part

--- driving.py
+++ driving.py
@@ -1,18 +1,20 @@
 country = input('Please input your country: ')
 age = input('Please input your age: ')
 age = int(age)
-if country == 'Taiwan' or 'TW':
+if country == 'Taiwan':
 	if age >= 18:
 		print('You can own a driving license')
 	else:
 		print('You cannot own a driving license')
-elif country == 'Indonesia' or 'IDN':
+elif country == 'Indonesia':
 	if age >= 20:
 		print('You can own a driving license')
 	else:
 		print('You cannot own a driving license')
-elif country == 'USA' or 'US' or 'the US':
+elif country == 'USA':
 	if age >= 16:
 		print('You can own a driving license')
 	else:
 		print('You cannot own a driving license')
+else:
+	print('You can only input Taiwan, Indonesia or USA')
